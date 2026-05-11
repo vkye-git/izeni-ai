1187e8cf23c1f83deccb918d5b22853474c1ab20
vkye-git <vkyekl@gmail.com>
2026-05-11 07:19:48 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+module.exports = async function(req, res) {
+
+/* CORS */
 
 res.setHeader('Access-Control-Allow-Origin', '*')
 res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
@@ -36,10 +38,14 @@ return res.status(200).json(data)
 
 } catch(error){
 
+console.log(error)
+
 return res.status(500).json({
+
 error:{
 message:error.message
 }
+
 })
 
 }
