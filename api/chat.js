df313bf2f3a8981e6dc2403461abc4190618e1f2
vkye-git <vkyekl@gmail.com>
2026-05-11 07:04:12 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,5 @@
 export default async function handler(req, res) {
 
-/* CORS */
-
 res.setHeader('Access-Control-Allow-Origin', '*')
 res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
 res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
@@ -16,26 +14,17 @@ const userMessage =
 req.body.messages[req.body.messages.length - 1].content
 
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.Gemini_API_Key}`,
+'https://api-inference.huggingface.co/models/microsoft/DialoGPT-large',
 {
 method:'POST',
 
 headers:{
-'Content-Type':'application/json'
+'Content-Type':'application/json',
+'Authorization':`Bearer ${process.env.HF_TOKEN}`
 },
 
 body:JSON.stringify({
-
-contents:[
-{
-parts:[
-{
-text:userMessage
-}
-]
-}
-]
-
+inputs:userMessage
 })
 
 }
@@ -48,11 +37,9 @@ return res.status(200).json(data)
 } catch(error){
 
 return res.status(500).json({
-
 error:{
 message:error.message
 }
-
 })
 
 }
