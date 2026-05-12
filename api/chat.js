541c2739a42692e4447d32ff41cbc4f8efcdcec1
vkye-git <vkyekl@gmail.com>
2026-05-12 08:24:15 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -10,6 +10,25 @@ file with this complete updated code.
 
 ---
 
+  
+export default async function handler(req, res) {
+
+res.setHeader('Access-Control-Allow-Origin', '*')
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+if (req.method === 'OPTIONS') {
+return res.status(200).end()
+}
+
+if (req.method !== 'POST') {
+return res.status(405).json({
+success:false,
+message:'Method not allowed'
+})
+}
+  
+  
 ```javascript
 const systemPrompt = require('../knowledge/systemPrompt')
 const products = require('../knowledge/products.json')
